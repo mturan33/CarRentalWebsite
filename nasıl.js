cd7b8ef1f9a3ae8cdc4fe957655fdf48608ffa4e
kaan2210 <131893604+kaan2210@users.noreply.github.com>
2024-05-15 21:54:17 +0300

    sign in kısmı eklendi
    asd

--- nasıl.js
+++ nasıl.js
@@ -3,9 +3,15 @@
 function openLoginPopup() {
   document.getElementById('loginPopup').style.display = 'block';
 }
+function openSigninPopup() {
+	document.getElementById('signinPopup').style.display = 'block';
+}
 
 function closeLoginPopup() {
   document.getElementById('loginPopup').style.display = 'none';
+}
+function closeSigninPopup() {
+	document.getElementById('signinPopup').style.display = 'none';
 }
 
 function login() {
@@ -19,7 +25,36 @@ function login() {
   
   closeLoginPopup();
 }
+function signin() {
+	window.location.href = "baseuye.html";
+	isLoggedIn = true;
+	var newusername = document.getElementById('newusername').value;
+	document.getElementById('userProfile').style.display = 'block';
+    document.getElementById('usernameDisplay').textContent = username;
+	var password = document.getElementById("newpassword").value;
+    var email = document.getElementById("newemail").value;
+
+    // Şifre 8 karakterden uzun olmalı
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters long.");
+        return;
+    }
+
+    // E-posta içinde "@" sembolü olmalı
+    if (!email.includes("@")) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+
+    // Diğer gerekli işlemler buraya eklenebilir, örneğin form submit işlemi veya AJAX çağrısı
+    alert("Sign in successful!");
+	
+	closeSigninPopup();
+}
 function logout() {
+	window.location.href = "base.html";
+}
+function signout() {
 	window.location.href = "base.html";
 }
 window.onload = function() {
